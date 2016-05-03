c3a406642e7714b5a31ff22a094f0f60adadb4d4
Travis CI documentation builder <sergey.seroshtan@gmail.com>
2016-05-03 09:45:00 +0000

    Automated documentation build for changeset 3dd7e1ae8469e74754e72aaf7ec1e2d7a1b8f4d8.

--- v3.1/search/enumvalues_2.js
+++ v3.1/search/enumvalues_2.js
@@ -4,7 +4,6 @@ var searchData=
   ['virgil_5fcard_5fget',['VIRGIL_CARD_GET',['../classvirgil_1_1sdk_1_1_error.html#ac848dd04b2755760e31c028bbe67d3d2a9acc9b230b78758117bbdc3063e80fc2',1,'virgil::sdk::Error']]],
   ['virgil_5fcard_5frevoke',['VIRGIL_CARD_REVOKE',['../classvirgil_1_1sdk_1_1_error.html#ac848dd04b2755760e31c028bbe67d3d2a2a97be9f4216eb07c2bbf57c9f79dbb8',1,'virgil::sdk::Error']]],
   ['virgil_5fcard_5fsearch',['VIRGIL_CARD_SEARCH',['../classvirgil_1_1sdk_1_1_error.html#ac848dd04b2755760e31c028bbe67d3d2a30d5c32b8e51dfe1fc7c16e699150126',1,'virgil::sdk::Error']]],
-  ['virgil_5fcard_5fsearch_5fapp',['VIRGIL_CARD_SEARCH_APP',['../classvirgil_1_1sdk_1_1_error.html#ac848dd04b2755760e31c028bbe67d3d2ac06422a4a0b81818e395c6cd8a4cade2',1,'virgil::sdk::Error']]],
-  ['virgil_5fcard_5fsign',['VIRGIL_CARD_SIGN',['../classvirgil_1_1sdk_1_1_error.html#ac848dd04b2755760e31c028bbe67d3d2a9b30ec3a5bf5d64f706cf9cdcedc444e',1,'virgil::sdk::Error']]],
-  ['virgil_5fcard_5funsign',['VIRGIL_CARD_UNSIGN',['../classvirgil_1_1sdk_1_1_error.html#ac848dd04b2755760e31c028bbe67d3d2a6cb022424e6d87762c7f06e73c150887',1,'virgil::sdk::Error']]]
+  ['virgil_5fcard_5fsearch_5fglobal',['VIRGIL_CARD_SEARCH_GLOBAL',['../classvirgil_1_1sdk_1_1_error.html#ac848dd04b2755760e31c028bbe67d3d2a6b54ef024afe92ca39e834a6ed0ae7ce',1,'virgil::sdk::Error']]],
+  ['virgil_5fcard_5fsearch_5fglobal_5fby_5femail',['VIRGIL_CARD_SEARCH_GLOBAL_BY_EMAIL',['../classvirgil_1_1sdk_1_1_error.html#ac848dd04b2755760e31c028bbe67d3d2a4473aa2db61ae076ace9671df418422b',1,'virgil::sdk::Error']]]
 ];
--- v3.1/search/functions_0.js
+++ v3.1/search/functions_0.js
@@ -3,5 +3,5 @@ var searchData=
   ['add',['add',['../classvirgil_1_1sdk_1_1client_1_1_private_key_client.html#a6c6708bd119f2adfe0a5149852ed1fc9',1,'virgil::sdk::client::PrivateKeyClient']]],
   ['asnumber',['asNumber',['../classvirgil_1_1sdk_1_1_version.html#a7dfb2d52cd451847197ae282aa2c3792',1,'virgil::sdk::Version']]],
   ['asstring',['asString',['../classvirgil_1_1sdk_1_1_version.html#ae438a19cf52cfbeb938d7e61d92d065b',1,'virgil::sdk::Version']]],
-  ['authorizedby',['authorizedBy',['../classvirgil_1_1sdk_1_1models_1_1_card_model.html#a5138aebaf3dc4ac40018dddaaf61b6ce',1,'virgil::sdk::models::CardModel::authorizedBy()'],['../classvirgil_1_1sdk_1_1models_1_1_identity_model.html#ac7b45561b80289a48ac781892d118ef5',1,'virgil::sdk::models::IdentityModel::authorizedBy()']]]
+  ['authorizedby',['authorizedBy',['../classvirgil_1_1sdk_1_1models_1_1_card_model.html#a268f5d20f1e6aaa41f0f4bf042388a78',1,'virgil::sdk::models::CardModel']]]
 ];
